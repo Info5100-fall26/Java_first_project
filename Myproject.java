d7412549a77bd3fe9efc6abe0cef1f35e6218340
SHHV308 <shrinidhihv05@gmail.com>
2026-09-24 14:02:53 -0700

    Homework3

---
+++ Myproject.java
@@ -0,0 +1,14 @@
+package edu.neu.mgen;
+
+/**
+ * Hello world!
+ * this is my first java projectt
+ *
+ */
+public class Myproject 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "It is my first Java program" ); //print statement for the java program
+    }
+}
